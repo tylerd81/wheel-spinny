2b3a2696d8c8cfcdd252e6e047c03b211797a389
tylerd81 <drelyt@gmail.com>
2019-04-03 09:46:53 -0400

    got rid of text only version

--- spinner.js
+++ spinner.js
@@ -1,3 +1,5 @@
+// tyler d 4/1/2019
+
 let power = 0;
 let max = 130; // how many degrees per tick it will spin
 let min = 50; // once it hits min it will stop spinning
@@ -20,20 +22,27 @@ let items = [
     {language: "C#", position: 290},
 ];
 
-function findClosestItem(position) {
-    let top = 270; // top position of wheel is at 270 degrees
-                   // so whichever item is closest to 270 wins.
-
+function findClosestItem(position) {    
     let currentWinner = 0;
     let closestSoFar = 360;    
 
     for(let currItem = 0; currItem < items.length; currItem++) {
         let item = items[currItem];
 
+        //every item shifts by however many degrees the wheel has spun
         let itemPos = item.position + position;
         if(itemPos > 360) {
-            itemPos -= 360;
+            itemPos -= 360; // clamp items between 0 and 360
         }
+
+        // the item that has the closest distance to 270 (which is the top of 
+        // wheel where the marker is) is the winner.
+
+        // 270 is the top of the wheel because of the way the css rotate transform
+        // works. For an item in the circle starting at the 3 o'clock position (
+        // which is a rotation of 0 degrees or 360 degrees) to reach the top of 
+        // the circle (12 o'clock) it has to rotate 270 degrees. The winning item
+        // is at the 12 o'clock position on the circle.
 
         let distance = Math.abs(itemPos - 270);
         if(distance < closestSoFar) {
@@ -46,7 +55,6 @@ function findClosestItem(position) {
     return items[currentWinner];
 }
 
-
 function spin() {
     if(isSpinning) {
         return; // only spin once
@@ -55,18 +63,21 @@ function spin() {
     }
 
     power = Math.floor(Math.random() * max);
+
+    // make sure the wheel spins at least minimum speed
     if(power < min) {
         power = (min - power) + min;
     }
 
     let timerId = setInterval(() => {
-        displayPower();
+        // displayPower();
         currentPos += power;
         if(currentPos > 360) {
             currentPos -= 360;
         }
 
         spinner.style.transform = `rotate(${currentPos}deg)`;
+
         power -= decAmt;
         if(power <= 0) {
             clearInterval(timerId);
